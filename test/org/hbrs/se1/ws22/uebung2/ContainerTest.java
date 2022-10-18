0a80021e8f67dde1d79441b21dfd26fbedce5aa7
Dlayzuchiha <danny-tran@hotmail.de>
2022-10-18 12:11:27 +0200

    Hinzugefügt ContainerTest

---
+++ test/org/hbrs/se1/ws22/uebung2/ContainerTest.java
@@ -0,0 +1,22 @@
+package org.hbrs.se1.ws22.uebung2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerTest {
+
+    @org.junit.jupiter.api.Test
+    void addMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteMember() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dump() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+}
